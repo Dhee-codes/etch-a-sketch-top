46f0854bc48a89b4388cfac758fcd13b6d23b933
Dhee-codes <divine.e.obiorah@gmail.com>
2025-10-08 17:00:32 +0100

    UI tweaks

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,25 @@
-const container = document.querySelector(".container");
+const sketchPad = document.querySelector(".sketch-pad");
+const option = document.querySelectorAll("option");
+
+function getPadLayout() {
+    let layout = option.value;
+    return layout;
+}
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
-        const box = document.createElement("div");
-        container.append(box);
+        const grid = document.createElement("div");
+        grid.classList.add("pixel");
+        sketchPad.append(grid);
     }
 }
+
+const pixels = document.querySelectorAll(".pixel");
+
+pixels.forEach(pixel => {
+  pixel.addEventListener("mouseenter", () => {
+    pixel.style.backgroundColor = "blue";
+  });
+});
+
+console.log(getPadLayout());
